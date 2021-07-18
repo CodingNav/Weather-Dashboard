f7819b0ec89c718ae7861d5aa2db6b4b342b5b6a
CodingNav <navleenkaurr@gmail.com>
2021-07-18 19:51:59 -0400

    Set up current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 var APIKey  = "4383960b162385ee11decc2446137670";
 var city = "new+york";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + APIKey;
+
+var cityDisplay = document.querySelector("#city-name");
+var iconDisplay = document.querySelector("#icon");
+var tempDisplay = document.querySelector("#temp");
+var windDisplay = document.querySelector("#wind");
+var humidityDisplay = document.querySelector("#humidity");
+var indexDisplay = document.querySelector("#uv-index");
 
 // https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=4383960b162385ee11decc2446137670
 
@@ -9,6 +16,13 @@ fetch(queryURL)
     return response.json();
   })
   .then(function (data) {
+    cityDisplay.innerHTML = data.name;
+    iconDisplay.src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+    tempDisplay.innerHTML = data.main.temp;
+    windDisplay.innerHTML = data.wind.speed;
+    humidityDisplay.innerHTML = data.main.humidity;
     console.log(data);
   });
+
+
 
