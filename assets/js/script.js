220c11e99aef030a76da5877898e251d8b5a265d
CodingNav <navleenkaurr@gmail.com>
2021-07-18 19:24:17 -0400

    Set up the API request

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var APIKey  = "4383960b162385ee11decc2446137670";
+var city = "new+york";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+// https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=4383960b162385ee11decc2446137670
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
